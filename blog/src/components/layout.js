2f5be8eb1a85135363ba560850971bffd85068d0
David Stupek <davidstupek@Davids-Air-2.lan>
2024-02-26 20:06:55 -0600

    Corrected file structure, server starts,  but theme still seems off... time to tinker.

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -9,6 +9,8 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
+import { ThemeProvider } from 'styled-components';
+
 
 import Header from "./header"
 import "./layout.css"
