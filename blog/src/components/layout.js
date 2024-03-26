8d5ffc92550ce0895a8c1eb84fed150f46f49307
David Stupek <davidstupek@Davids-Air-2.lan>
2024-03-26 07:52:40 -0500

    reorganized

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import styled, { ThemeProvider } from "styled-components"
 
 
-import Header from "./header"
+import { Header } from "./Header"
 import "./layout.css"
 import { Gray } from "./themes/Gray"
 
