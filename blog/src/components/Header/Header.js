0fcaba425398f5cd3496fc61a7f088b277231b52
David Stupek <davidstupek@Davids-Air-2.lan>
2024-04-11 17:42:55 -0500

    headerUpdated

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
-
+import { H1 } from "../Heading"
 
 const StyledHeader = styled.header`
   margin: 0 auto;
@@ -22,7 +22,9 @@ const StyledLink = styled(Link)`
 const Header = ({ siteTitle }) => (
   <StyledHeader>
     <StyledLink to="/">
+      <H1>
       {siteTitle}
+      </H1>
     </StyledLink>
   </StyledHeader>
 )
